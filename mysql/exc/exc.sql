3f84c5b228b2363d4a3cbff16352850cad31f3c1
twino <twino2@naver.com>
2022-01-24 11:52:07 +0900

    mysql

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,20 @@
+use world;
+
+show tables;
+
+-- 주석 --
+
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+-- 테이블 삭제 --
